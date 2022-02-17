61a5ac0066517d4cbe306a0d797b48b8bb9544fa
Martin <901824+martinstark@users.noreply.github.com>
2022-02-17 15:54:17 +0100

    feat: extend metadata event, update init event
    Now that there is a canonical home for non-QoS metadata,
    move metadata from the init event to the metadata event.

--- specification/typescript/event/init.d.ts
+++ specification/typescript/event/init.d.ts
@@ -1,16 +1,5 @@
 import { TBaseEvent } from "./base";
 
-export type TInitEventPayload = Record<string, any> & {
-  contentId: string;
-  contentUrl: string;
-  drmType?: string;
-  userId?: string;
-  deviceId?: string;
-  deviceModel?: string;
-  deviceType?: string;
-}
-
 export type TInitEvent = TBaseEvent & {
   event: "init";
-  payload: TInitEventPayload;
 }
